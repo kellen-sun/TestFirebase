1a235f2546838ce982fe24546cb67eb523ff0806
kellen-sun <ksun5@ocdsb.ca>
2022-11-26 16:58:40 -0500

    update

--- index.js
+++ index.js
@@ -11,4 +11,13 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function (req, res, next) {
+    res.sendFile('C:\Users\sunke\Desktop\Kellen\Programming\Javascript\TestFirebase\main.html');
+});
+app.post('/', function (req, res, next) {
+    console.log(req.body);
+    register(req, res, next);
+    res.send('User created successfully');
+});
+
 app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
