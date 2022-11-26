cc81d3b443fd40780eb122549d6d304b9057f240
kellen-sun <ksun5@ocdsb.ca>
2022-11-26 16:39:01 -0500

    creating firebase test

--- config.js
+++ config.js
@@ -5,3 +5,18 @@ const {PORT, HOST, HOST_URL, API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, M
 
 assert(PORT, ‘PORT is required’);
 assert(HOST, ‘HOST is required’);
+module.exports = {
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    
+    firebaseConfig: {
+            apiKey: API_KEY,
+            authDomain: AUTH_DOMAIN,
+            projectId: PROJECT_ID,
+            storageBucket: STORAGE_BUCKET,
+            messagingSenderId: MESSAGIN_SENDER_ID,
+            appId: APP_ID,
+        }
+    }
+    
