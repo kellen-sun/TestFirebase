24e2af46e4438e4acec415fd07a3ac39d349ea36
kellen-sun <ksun5@ocdsb.ca>
2022-11-30 09:36:22 -0500

    addming more functions

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,11 @@
+'use strict';
+
 const firebase = require('../db');
 const User = require('../models/users');
 const firestore = firebase.firestore();
-const addUser = async (req, res, next) => {
+const { openDelimiter } = require('ejs');
+
+const createUser = async (req, res, next) => {
     try {
         const data = req.body;
         await firestore.collection('users').doc().set(data);
@@ -10,4 +14,90 @@ const addUser = async (req, res, next) => {
         console.log(error.message);
     }
 };
-module.exports = {addUser};
+
+const updateUser = async (req, res, next) => {
+    try {
+        const data = req.body;
+        console.log(data);
+        var output = [false, 'Email not found!'];
+        const user = await firestore.collection('users').where("email", "==", data.email)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (doc.data().password == data.oldPassword) {
+                        doc.ref.update(
+                            {
+                                'email': data.email,
+                                'password': data.newPassword,
+                                'username': data.newUsername
+                            }
+                        );
+                        output = [true];
+                    }
+                    else {
+                        output = [false, 'Incorrect Password'];
+                    }
+                });
+            });
+        
+        return(output);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteUser = async (req, res, next) => {
+    try {
+        const data = req.body;
+        var output = [false, 'Email not found!'];
+        const user = await firestore.collection('users').where("email", "==", data.email)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (doc.data().password == data.password) {
+                        doc.ref.delete();
+                        output = [true];
+                    }
+                    else {
+                        output = [false, 'Incorrect Password'];
+                    }
+                });
+            });
+        return(output);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const readUser = async (req, res, next) => {
+    try {
+        const data = req.body;
+        var output = [false, 'Email not found!'];
+        const user = await firestore.collection('users').where("email", "==", data.email)
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    if (doc.data().password == data.password) {
+                        output = [true, doc.data()];
+                    }
+                    else {
+                        output = [false, 'Incorrect Password'];
+                    }
+                });
+            });
+        return(output);
+    }
+    catch (error) {
+        //res.status(400).send(error.message);
+    }
+};
+
+
+module.exports = {
+    createUser,
+    readUser,
+    updateUser,
+    deleteUser
+};
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
-const {addUser} = require('./controllers/UserController');
+const {createUser} = require('./controllers/UserController');
 
 const app = express(); //this is our application/website, which is important
 app.set('view engine', 'html');
@@ -17,21 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //makes the static files accessible to the webpages
 app.use(express.static('C:/Users/sunke/Desktop/Kellen/Programming/Javascript/TestFirebase/static'));
 
-/*
-app.get('/home', function (req, res, next) {
-    //res.render('C:/Users/sunke/Desktop/Kellen/Programming/Javascript/TestFirebase/templates/home.html');
-});
-
-app.get('/newUser', function (req, res, next) {
-    res.render('C:/Users/sunke/Desktop/Kellen/Programming/Javascript/TestFirebase/templates/newUser.html');
-});
-*/
 app.post('/newUser', function (req, res, next) {
     var data = req.body.username;
     console.log(data);
     //console.log(req);
     //this function adds the data to the database
-    addUser(req, res, next);
+    createUser(req, res, next);
     //renders the newuser webpage with the data
     res.render('C:/Users/sunke/Desktop/Kellen/Programming/Javascript/TestFirebase/templates/newUser.html', { username: data });
 });
