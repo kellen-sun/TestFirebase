429e002ca985c90ba32e5aec103570fe4c104f04
kellen-sun <ksun5@ocdsb.ca>
2022-11-30 20:54:12 -0500

    format

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const dotenv = require('dotenv') //used to get the stuff from .env
 const assert = require('assert')
 dotenv.config();
-const {PORT, HOST, HOST_URL, API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} = process.env; //creates and sets these variables to the stuff we made in .env
+const {PORT, HOST, HOST_URL, API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID} = process.env;
+//creates and sets these variables to the stuff we made in .env
 
 //assert(PORT, 'PORT is required');
 //assert(HOST, 'HOST is required');
